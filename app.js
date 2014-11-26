60643f28e258cb022a7197da47ceca9e21b32b2d
dwn <dwendt@knights.ucf.edu>
2014-11-25 22:46:04 -0500

    initial initialization code

--- app.js
+++ app.js
@@ -1,11 +1,72 @@
 "use strict";
 
-var fs		= require('fs'),
-		nconf = require('nconf');
+var fs    = require('fs'),
+    nconf = require('nconf');
 
-// nconf should use command-line args, then env vriables, then a config file.
+// nconf should use command-line args, then env vriables. 
 nconf.argv().env();
 
 global.env = process.env.NODE_ENV || 'production';
 
 
+function loadConfig() {
+  nconf.file({
+    file: configFile
+  });
+
+  // default ir2 settings.
+  nconf.defaults({
+    base_dir: __dirname,                                  // just where everything is at.
+    files_location: path.join(__dirname, 'uploads'),      // path to store uploaded files for analysis in
+    views_dir: path.join(__dirname, 'public/templates')   // handlebars templates to render
+  });
+}
+
+
+function start() {
+  loadConfig();
+
+  var templates = require("./src/templates"),
+      webserver = require("./src/webserver"),
+      sockets = require("./src/socket.io"),
+      r2piper   = require("r2piper");
+
+  templates.setGlobal('relative_path', nconf.get('relative_path'));
+
+
+  r2piper.init(function(err) {
+    if (err) {
+      winston.error("[app] r2piper failed to initialize.");
+      winston.error(err);
+      return;
+    }
+
+    // Continue app init
+    sockets.init(webserver.server);
+
+    nconf.set('url', nconf.get('base_url') +
+                    (nconf.get('use_port') ? ':'+nconf.get('port') : '') +
+                     nconf.get('relative_path') );
+
+    templates.compile();
+    webserver.listen();
+
+    process.on('SIGTERM', shutdown);
+    process.on('SIGINT', shutdown);
+  });
+}
+
+
+function shutdown(code) {
+  winston.info('[app] Shutdown initialized.');
+
+  _.each(require('./src/webserver').servers, function(server) {
+    server.close();
+  });
+
+  // TODO: handle killing r2pipe stuff
+
+  winston.info('[app] Shutdown complete.');
+
+  process.exit(code || 0);
+}
